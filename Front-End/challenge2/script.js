2bccdb2eec3c90bb7cd271a3a22ee2072c3eb7a5
marjo1994 <m_rhl@live.com>
2025-07-18 10:52:37 -0500

    feat(calculator): initialize project and implement number display

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -1,7 +1,43 @@
-/*
-TO-DO:
+const showResult = document.getElementById('display');
+const numberButtons = document.querySelectorAll('button:not(.operation-btn)');
+const operationButtons = document.querySelectorAll('.operation-btn');
 
-- Modify this file only
-- The calculator should be completely functional
+const calculatorState = {
+  input: '0',
+  firstNumber: null,
+  operator: null,
+  waitingForSecondNumber: false,
+};
 
-*/
+const initCalculator = () => {
+  setupEventListeners();
+  updateResult();
+}
+
+const updateResult = () => {
+  showResult.textContent = calculatorState.input;
+}
+
+const setupEventListeners = () => {
+  //NumberButtons
+  numberButtons.forEach(btn => btn.addEventListener('click', () => handleNumberButtons(btn)));
+  //OperationButtons
+}
+
+const handleNumberButtons = (btn) => {
+    const { input, waitingForSecondNumber } = calculatorState;
+
+    const number = btn.textContent;
+        
+    if (waitingForSecondNumber) {
+        calculatorState.input = number;
+        calculatorState.waitingForSecondNumber = false;
+    } else {
+        calculatorState.input = input === '0' ? number : input + number;
+    }
+    updateResult();
+}
+
+initCalculator();
+
+
